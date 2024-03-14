aa12bc0003af5bdbe6cf720bc984246a6eba5bf6
Gabrielly Oliveira <oliveiragabrielly.dev@gmail.com>
2024-03-14 14:21:00 -0300

    Teste de variável

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,6 +2,12 @@ public class MinhaClasse {
 
   public static void main(String[] args) {
     System.out.print("Olá, aqui é a Gabrielly aprendendo Java.");
+
+    String meuNome = "Gabrielly";
+    int anoFabricacao = 2022;
+
+    boolean verdadeira = false;
+
   }
 
 }
