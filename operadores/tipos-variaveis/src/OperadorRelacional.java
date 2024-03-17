b1018e47f62715625b27dd61a0ce35874eecd704
Gabrielly Oliveira <oliveiragabrielly.dev@gmail.com>
2024-03-17 14:58:24 -0300

    Relacionais

---
+++ operadores/tipos-variaveis/src/OperadorRelacional.java
@@ -0,0 +1,12 @@
+public class OperadorRelacional { 
+
+  public static void main(String[] args) {
+    int numeroA = 1;
+    int numeroB = 2;
+
+    boolean simNao = numeroA == numeroB;
+
+    System.out.println("Número A é igual a Numero B?" + simNao);
+  }
+  
+}
